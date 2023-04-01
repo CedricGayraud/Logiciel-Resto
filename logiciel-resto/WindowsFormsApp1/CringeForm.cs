fac9f736c2024cf3e3b59790dfba29f694a7371e
Cedric Gayraud <cedric@agence-greenweb.com>
2023-04-01 22:39:23 +0200

    connexion base de données

--- logiciel-resto/WindowsFormsApp1/CringeForm.cs
+++ logiciel-resto/WindowsFormsApp1/CringeForm.cs
@@ -2,16 +2,21 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace WindowsFormsApp1
 {
     public partial class CringeForm : Form
     {
+
+        MySqlConnection connection = new MySqlConnection("database=restaurant_manager; server=localhost; user id=root; password=");
+
         public CringeForm()
         {
             InitializeComponent();
@@ -22,9 +27,30 @@ namespace WindowsFormsApp1
             
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void TestBtn_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("HUH???");
+            try
+            {
+                string deleteQuery = "DELETE FROM plats WHERE idPlat = " + textBoxID.Text;
+                connection.Open();
+                MySqlCommand command = new MySqlCommand(deleteQuery, connection);
+
+                if (command.ExecuteNonQuery() == 1)
+                {
+                    MessageBox.Show("USER DELETED");
+                }
+                else
+                {
+                    MessageBox.Show("USER NOT DELETED");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            connection.Close();
+
         }
 
         private void editionToolStripMenuItem_Click(object sender, EventArgs e)
